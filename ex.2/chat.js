1b54d1bcfacb81bbab525b0b448735ea118a7972
uneven39 <unwar39@gmail.com>
2017-10-01 20:34:05 +0200

    ex.2 - EventEmitters

--- ex.2/chat.js
+++ ex.2/chat.js
@@ -15,6 +15,7 @@ class ChatApp extends EventEmitter {
   }, 1000);
   }
 
+  // 2.1 - В классе чата ChatApp добавить метод close
   close() {
     this.emit('close', this.title);
   }
@@ -28,29 +29,36 @@ let chatOnMessage = (message) => {
   console.log(message);
   console.log('');
 };
-
+// 1.1 - доп. обработчик
 function prepareToAnswer() {
 	console.log('Готовлюсь к ответу');
 }
 
+// 2.2 - обработчик закрытия
 function chatOnClose(chatName) {
   console.log(`Чат ${chatName} закрылся :(`);
 }
 
+
+// 1.1 - Добавить обработчик события message для Чата Вебинара (webinarChat)
 webinarChat.on('message', prepareToAnswer);
 webinarChat.on('message', chatOnMessage);
 
 facebookChat.on('message', chatOnMessage);
 
+// 1.2 - Для Вконтакте (vkChat) установить максимальное количество обработчиков событий, равное 2
 vkChat.setMaxListeners(2);
+// 1.3 - Добавить обработчик 'Готовлюсь к ответу' из пункта 1.1 к чату Вконтакте
 vkChat.on('message', prepareToAnswer);
 vkChat.on('message', chatOnMessage);
+// 2.2 - Для чата вконтакте (vkChat) добавить обработчик close
 vkChat.on('close', chatOnClose);
 
 
 // Закрыть вконтакте
 setTimeout( ()=> {
   console.log('Закрываю вконтакте...');
+  // 2.3 - Вызывать у чата вконтакте метод close()
   vkChat.close();
   vkChat.removeListener('message', chatOnMessage);
   vkChat.removeListener('message', prepareToAnswer);
